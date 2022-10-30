b329fc03a4b8becc91a2ca00b8c1d834a9010b80
Juan Del Rio <52029674+juandrh@users.noreply.github.com>
2022-10-30 12:52:15 +0100

    Add files via upload

--- Constructores/factoryMethod_VirtualConstructor/CreadorDeDocumentos.java
+++ Constructores/factoryMethod_VirtualConstructor/CreadorDeDocumentos.java
@@ -5,9 +5,11 @@ public abstract class CreadorDeDocumentos {
 	public CreadorDeDocumentos() {
 		// TODO Auto-generated constructor stub
 	}
+
 	public abstract Documento crearDocumento();
+
 	public void imprimirDocumento() {
-	Documento documento = crearDocumento();
-	documento.imprimir();
+		Documento documento = crearDocumento();
+		documento.imprimir();
 	}
 }
